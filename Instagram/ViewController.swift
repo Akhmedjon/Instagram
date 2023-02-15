98b19dd0237f65d2619c22bf2cacc31fa33a043f
Ahmedjon <boltaevahmedjon28@gmail.com>
2023-02-15 14:09:09 +0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ахмеджон Болтаев on 15/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
